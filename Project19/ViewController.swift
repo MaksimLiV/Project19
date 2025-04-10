fe27107f95be9fb5b9442f02e28768cf1f105107
Maksim Li <maximlee.ml@gmail.com>
2025-04-10 22:37:01 +0300

    Initial Commit

---
+++ Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Maksim Li on 08/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
